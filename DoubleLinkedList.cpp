edb5b604583f8c272eebbaa380ad55c18b73d767
restuchan <restu.chan188@gmail.com>
2025-05-15 10:33:56 +0700

    step 1

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -94,4 +94,14 @@ class DoubleLinkedList
 
         Node *current = START;
 
+        //step 1:Traverse the list to find the node 
+        while(current != NULL && current->noMhs != rollNo)
+            current = current->next;
+
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
+
         
