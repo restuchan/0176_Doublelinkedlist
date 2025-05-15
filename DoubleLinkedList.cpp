daf4fe1bc48bcabc0a2fadf0f190d246857527fc
restuchan <restu.chan188@gmail.com>
2025-05-15 10:41:03 +0700

    step 3

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -113,4 +113,7 @@ class DoubleLinkedList
         }
         else
         {
-            
+           //step 3: link previous node to next of current
+           current->prev->next = current->next;
+
+           
