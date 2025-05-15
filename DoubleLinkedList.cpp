08a75bc6ab98f1b1dabeb005c461f8fdb4f4cba2
restuchan <restu.chan188@gmail.com>
2025-05-15 10:01:58 +0700

    MENAMBAHKAN VOID ADDNODE

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -21,4 +21,11 @@ class DoubleLinkedList
         START = NULL;
     }
 
-    
+    void addNode()
+    {
+        int nim;
+        string nm;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+
+        
