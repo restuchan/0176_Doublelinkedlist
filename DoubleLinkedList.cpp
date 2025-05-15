00337d38fd1a5b7d4f39a5a52b58670ca0a5c850
restuchan <restu.chan188@gmail.com>
2025-05-15 11:07:07 +0700

    void retraverse

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -148,6 +148,34 @@ class DoubleLinkedList
        }
    }
 
-}; 
 
+    void retraverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
 
+        //step 1: Move to last node 
+        Node *currentNode = START;
+        int i = 0;
+        while(currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+            i++;
+        }
+
+        //step 2: Travers backward
+        cout << "\nRecords is descanding order of roll number are:\n";
+        while (currentNode != NULL)
+        {
+            cout << 1 + 1 << ". " << currentNode->noMhs << " " << endl;
+
+            //step 3: Move to previous node 
+            currentNode = currentNode->prev;
+            i++;
+        }
+    }
+
+    
