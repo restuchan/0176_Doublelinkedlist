b82eaab0201aff787e32bcbffab31e32ac0a81c3
restuchan <restu.chan188@gmail.com>
2025-05-15 10:22:13 +0700

    step 9

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -71,4 +71,8 @@ class DoubleLinkedList
             return;
         }
 
+        //step 9: Insert between current and current->next
+        newNode->next = current->next; //step 9a: newNode.next = current.next
+        newNode->prev = current;       //step 9b: newNode.prev = current
+
         
