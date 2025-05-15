02392969c4b062cec2f881d248ba6e88a868ecfa
restuchan <restu.chan188@gmail.com>
2025-05-15 10:40:36 +0700

    step 2

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -104,4 +104,13 @@ class DoubleLinkedList
             return;
         }
 
-        
+        //step 2: if node is at the beginning 
+        if (current == START)
+        {
+            START = current->next; //step 2a: START=START.next
+            if (START != NULL)
+                START->prev = NULL; //step 2b : START.prev = NULL
+        }
+        else
+        {
+            
