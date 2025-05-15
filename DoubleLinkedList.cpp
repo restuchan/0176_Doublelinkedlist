88690194ddf957c1fc43b775175526676dd9d7a4
restuchan <restu.chan188@gmail.com>
2025-05-15 10:03:45 +0700

    step 4

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -42,4 +42,7 @@ class DoubleLinkedList
                 cout << "\nDuplicate number not allowed" << endl;
                 return;
             }
+            //Step 4: newNode.next = START
+            newNode->next = START;
+
             
