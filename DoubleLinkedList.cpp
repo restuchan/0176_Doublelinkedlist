127600121727d6253e50d22cfec29989ac1c38fd
restuchan <restu.chan188@gmail.com>
2025-05-15 10:21:53 +0700

    step 8

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -55,4 +55,20 @@ class DoubleLinkedList
             //step 7: START = newNode
             START = newNode;
             return;
+        }
+
+        //insert in between node
+        //step 8: locate position for insertion 
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
+
         
